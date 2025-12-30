ca1f91b11f5bbdbb0e23b176f12d5fd834e6be1a
Prateek Kumawat <prateekkumawat20@gmail.com>
2025-12-30 11:18:16 +0530

    add python ci

--- app/setup.py
+++ app/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dempapp",
-    version="2.0.0",
+    name="highskyapp",
+    version="4.0.1",
     packages=find_packages(),
     install_requires=[
         "requests",
